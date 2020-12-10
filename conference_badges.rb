fb827228191cf2d203f010ac6d042548dc844e79
Ben Hayes <b.t.hayes@comcast.net>
2020-12-10 20:38:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  new_array = []
   array.each do |name|
-    puts badge_maker(name)
+    new_array << badge_maker(name)
   end
+  new_array
 end
